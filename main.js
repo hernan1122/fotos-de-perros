932085ee38f14d04b8dbed7cf2eeab43d1e728a5
Axel Torrico <torricohernan2@gmail.com>
2022-10-11 12:56:42 -0300

    agregue formulario

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
+const api = axios.create({
+  baseURL: 'https://api.thedogapi.com/v1/'
+});
+api.defaults.headers.common['X-API-KEY'] = 'live_RxGOXR4gBPw0zxHL0ZqMwaCXjuDSNpVif2bw8igbOT5mn8fMP2Ld6wG8wAeUwe6N';
+
 const API_URL_RANDOM =
-  "https://api.thedogapi.com/v1/images/search?limit=3&api_key=live_RxGOXR4gBPw0zxHL0ZqMwaCXjuDSNpVif2bw8igbOT5mn8fMP2Ld6wG8wAeUwe6N";
+  "https://api.thedogapi.com/v1/images/search?limit=3";
 // limit=3 es un query parameters, estamos limitando la cantidad de imagenes
 const API_URL_FAVORITES =
-  "https://api.thedogapi.com/v1/favourites/?api_key=live_RxGOXR4gBPw0zxHL0ZqMwaCXjuDSNpVif2bw8igbOT5mn8fMP2Ld6wG8wAeUwe6N";
+  "https://api.thedogapi.com/v1/favourites";
 const API_URL_FAVORITES_DELETE = (id) =>
-  `https://api.thedogapi.com/v1/favourites/${id}?api_key=live_RxGOXR4gBPw0zxHL0ZqMwaCXjuDSNpVif2bw8igbOT5mn8fMP2Ld6wG8wAeUwe6N`;
+  `https://api.thedogapi.com/v1/favourites/${id}`;
+const API_URL_UPLOAD = `https://api.thedogapi.com/v1/images/upload`;
 
 const spanError = document.getElementById("error");
 
@@ -31,11 +37,23 @@ async function loadRandomImg() {
     btn1.onclick = () => saveFavouriteImg(data[0].id);
     btn2.onclick = () => saveFavouriteImg(data[1].id);
     btn3.onclick = () => saveFavouriteImg(data[2].id);
+
+    btn1.addEventListener("click", heartRed);
   }
 }
 
+function heartRed() {
+  btn1.classList.toggle("fa-regular");
+  console.log("hola");
+}
+
 async function loadFavouritesImg() {
-  const res = await fetch(API_URL_FAVORITES);
+  const res = await fetch(API_URL_FAVORITES, {
+    method: 'GET',
+    headers: {
+      'X-API-KEY': 'live_RxGOXR4gBPw0zxHL0ZqMwaCXjuDSNpVif2bw8igbOT5mn8fMP2Ld6wG8wAeUwe6N'
+    }
+  });
   const data = await res.json();
   console.log("favorites");
   console.log(data);
@@ -66,22 +84,26 @@ async function loadFavouritesImg() {
 }
 
 async function saveFavouriteImg(id) {
-  const res = await fetch(API_URL_FAVORITES, {
+  const {data, status} = await api.post('/favourites', {
+    image_id: id,
+  });
+
+  /* const res = await fetch(API_URL_FAVORITES, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      'X-API-KEY': 'live_RxGOXR4gBPw0zxHL0ZqMwaCXjuDSNpVif2bw8igbOT5mn8fMP2Ld6wG8wAeUwe6N',
     },
     body: JSON.stringify({
       image_id: id,
     }),
   });
-  const data = await res.json();
+  const data = await res.json(); */
 
   console.log("Save");
-  console.log(res);
 
-  if (res.status !== 200) {
-    spanError.innerHTML = "Hubo un error" + res.status + data.message;
+  if (status !== 200) {
+    spanError.innerHTML = "Hubo un error" + status + data.message;
   } else {
     console.log("Img guardada en favoritos");
     loadFavouritesImg();
@@ -91,6 +113,9 @@ async function saveFavouriteImg(id) {
 async function deleteFavouriteImg(id) {
   const res = await fetch(API_URL_FAVORITES_DELETE(id), {
     method: "DELETE",
+    headers: {
+      'X-API-KEY': 'live_RxGOXR4gBPw0zxHL0ZqMwaCXjuDSNpVif2bw8igbOT5mn8fMP2Ld6wG8wAeUwe6N',
+    }
   });
   const data = await res.json();
 
@@ -100,6 +125,34 @@ async function deleteFavouriteImg(id) {
     console.log("Img eliminada de favoritos");
     loadFavouritesImg();
   }
+}
+
+async function uploadImg() {
+  const form = document.getElementById('uploadingForm');
+  const formData = new FormData(form);
+
+  console.log(formData.get('file'));
+
+  const res = await fetch(API_URL_UPLOAD, {
+    method: 'POST',
+    headers: {
+      /* 'Content-Type': 'multipart/form-data', */
+      'X-API-KEY': 'live_RxGOXR4gBPw0zxHL0ZqMwaCXjuDSNpVif2bw8igbOT5mn8fMP2Ld6wG8wAeUwe6N',
+    },
+    body: formData,
+  })
+  const data = await res.json();
+
+  if (res.status !== 201) {
+        spanError.innerHTML = `Hubo un error al subir michi: ${res.status} ${data.message}`
+    }
+    else {
+        console.log("Foto de michi cargada :)");
+        console.log({ data });
+        console.log(data.url);
+        saveFavouriteImg(data.id)
+        //para agregar la img cargada a favoritos.
+    }
 }
 
 loadRandomImg();
